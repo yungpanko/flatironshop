2362b4344765bd34e76ca3122910df9a831946ce
yungpanko <jrjohns@mit.edu>
2017-06-20 14:47:10 -0400

    added flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: "You have successfully logged out."
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      #message => user wasn't creted? validations?
+      #reroute to new user path?
+      @user.errors.full_messages.each do |error|
+        flash.now[error] = error
+      end
+      render :new
+    end
     #may want to redirect_to index page instead (where all products are)
   end
 
