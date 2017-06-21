c94203903df2641d2802d4dd9f8794fe4d789143
Pat Rocchio <procchio6@gmail.com>
2017-06-21 09:39:32 -0400

    Add average rating to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ApplicationRecord
   has_many :orders, foreign_key: "buyer_id"
   has_many :items_bought, class_name:"Item", through: :orders, source: :items
   has_many :items_sold, class_name:"Item", foreign_key: "seller_id"
+  has_many :reviews, through: :items_sold
 
   validates :name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def average_rating
+    reviews.average(:rating).to_f
+  end
 
 end
