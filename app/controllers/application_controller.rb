a14b0420dc059641f2550cbe819b6dc25a0f3466
yungpanko <jrjohns@mit.edu>
2017-06-21 13:12:02 -0400

    item page checkout fix, validations, category filters, placeholder image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
