36e89813feaf26d012b8623d032fcbcd074cc39c
qtotuan <quynh.totuan@gmail.com>
2017-06-23 13:34:57 -0400

    Add sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,13 @@ require_relative 'application'
 Rails.application.initialize!
 
 require 'carrierwave/orm/activerecord'
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'flatironschool.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
